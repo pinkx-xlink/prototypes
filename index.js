f08ed0e980dccf4c37bef2b720c1b293636d5be0
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2023-07-07 10:10:23 -0400

    Comment out 40-41

--- index.js
+++ index.js
@@ -37,5 +37,5 @@ Player.prototype.sayHello = function() {
     console.log("Hello, I'm a player! ;p");
 }
 
-player1.sayHello() //logs "Hello, I'm a player"
-player2.sayHello() // logs "Hello, I'm a player"
+//player1.sayHello() //logs "Hello, I'm a player"
+//player2.sayHello() // logs "Hello, I'm a player"
