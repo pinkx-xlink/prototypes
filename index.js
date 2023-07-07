7c0f24dc94fc0da065bbad721306d79811ecd84e
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2023-07-06 21:37:55 -0400

    Add sayName function

--- index.js
+++ index.js
@@ -15,6 +15,9 @@ function printName(player) {
 function Player(name, marker) {
     this.name = name 
     this.marker = marker
+    this.sayName = function() {
+        console.log(name)
+    }
 }
 
 const player = new Player('steve', 'X')
